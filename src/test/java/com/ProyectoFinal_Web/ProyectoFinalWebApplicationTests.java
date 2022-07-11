1570298b09ca18a6ee154abb279db5f45a4ae25d
jala88 <cbarquero70286@ufide.ac.cr>
2022-07-10 23:03:56 -0600

    Se crea el form de login y hoja de estilo a utilizar

---
+++ src/test/java/com/ProyectoFinal_Web/ProyectoFinalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoFinal_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
